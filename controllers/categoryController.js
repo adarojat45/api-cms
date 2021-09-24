25131616bdd57ef61bcfd347d1e435e229f16204
Ajat Darojat <ajatdarojat45@gmail.com>
2021-09-24 10:44:47 +0700

    ad findAll endpoint

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -12,6 +12,21 @@ class CategoryController {
 			next(error);
 		}
 	};
+
+	static findAll = async (req, res, next) => {
+		try {
+			const condition = {};
+			const categories = await CategoryModel.findAll(condition);
+			const categoriesTransform = CategoryTransformer.list(categories);
+			res.status(200).json(categoriesTransform);
+		} catch (error) {
+			console.log(
+				"🚀 ~ file: categoryController.js ~ line 23 ~ CategoryController ~ findAll= ~ error",
+				categories
+			);
+			next(error);
+		}
+	};
 }
 
 module.exports = CategoryController;
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const CategoryController = require("../controllers/categoryController");
 
 router.post("/", CategoryController.create);
+router.get("/", CategoryController.findAll);
 
 module.exports = router;
