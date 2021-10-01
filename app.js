b0a2a826e26a64be5592573fa2b3ee50c07d82b1
Ajat Darojat <ajatdarojat45@gmail.com>
2021-10-01 14:14:11 +0700

    implement cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const router = require("./routes");
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
